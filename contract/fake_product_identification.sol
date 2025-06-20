69736ecbb89076378a519a2251a8bfd49fe58006
El-durado739 <swayammeshram09@gmail.com>
2025-06-20 16:26:36 +0530

    Update fake_product_identification.sol

--- contract/fake_product_identification.sol
+++ contract/fake_product_identification.sol
@@ -2,10 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/**
- * @title Fake Product Identification System
- * @dev This contract allows manufacturers to register products and users to verify authenticity.
- */
 contract FakeProductIdentificationSystem {
     address public owner;
 
